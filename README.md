55c864579331d03bae775d136c6c3eef15a4e4a7
alex <38814044+alextwothousand@users.noreply.github.com>
2021-06-13 21:22:06 +0100

    genesis

---
+++ README.md
@@ -0,0 +1,10 @@
+# altv-svelte-ts-boilerplate
+
+A simple Svelte & TypeScript boilerplate for AltV :heart:
+
+## How to use
+
+```
+npx degit alextwothousand/altv-svelte-ts-boilerplate my-project
+npm i
+```
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import type { RouteDefinition } from "svelte-spa-router";
+import Home from "./routes/Home.svelte";
+import Lorem from "./routes/Lorem.svelte";
+import NotFound from "./routes/NotFound.svelte";
+
+export default {
+	"/": Home,
+	"/lorem/:repeat": Lorem,
+	// The catch-all route must always be last
+	"*": NotFound
+} as RouteDefinition;
